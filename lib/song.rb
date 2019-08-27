4617363b4aeae7d4ce2132e8fd05a6977ea5443e
Austin Asbury <acasbury25@aol.com>
2019-08-27 12:58:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,13 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@artists = []
-  @@genre = []
+  @@genres = []
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
     @@artists << artist
+    @@genres << genre
   end
 end
