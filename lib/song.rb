2582e1713d90570e0a78d1422c22033996fc8ed1
Austin Asbury <acasbury25@aol.com>
2019-08-27 13:09:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,4 +34,15 @@ class Song
     unique_genres
   end
 
+  def self.genre_count
+    genre_hash = {}
+    @@genres.each do |genre|
+      if genre_hash.has_key?(genre)
+        genre_hash[genre] =+ 1
+      else
+        genre_hash[genre] = 1
+      end
+    end
+    genre_hash
+  end
 end
